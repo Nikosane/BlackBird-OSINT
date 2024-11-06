6367cbaf81c72a413ee16bfbcba75ed4bf9b4b46
Nikosane <linux.nikosane@gmail.com>
2024-11-06 23:08:59 +0530

    initiate function

--- blackbird.py
+++ blackbird.py
@@ -18,3 +18,135 @@ from modules.export.pdf import saveToPdf
 from modules.utils.file_operations import isFile, getLinesFromFile
 from modules.utils.permute import Permute
 from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def initiate():
+    if not os.path.exists("logs/"):
+        os.makedirs("logs/")
+    logging.basicConfig(
+        filename=config.LOG_PATH,
+        level=logging.DEBUG,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+
+    parser = argparse.ArgumentParser(
+        prog="blackbird",
+        description="An OSINT tool to search for accounts by username in social networks.",
+    )
+    parser.add_argument(
+        "-u",
+        "--username",
+        nargs="*",
+        type=str,
+        help="One or more usernames to search.",
+    )
+    parser.add_argument(
+        "-uf",
+        "--username-file",
+        help="The list of usernames to be searched.",
+    )
+    parser.add_argument(
+        "--permute",
+        action="store_true",
+        help="Permute usernames, ignoring single elements.",
+    )
+    parser.add_argument(
+        "--permuteall", action="store_true", help="Permute usernames, all elements."
+    )
+    parser.add_argument(
+        "-e",
+        "--email",
+        nargs="*",
+        type=str,
+        help="One or more email to search.",
+    )
+    parser.add_argument(
+        "-ef",
+        "--email-file",
+        help="The list of emails to be searched.",
+    )
+    parser.add_argument(
+        "--csv",
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="Generate a CSV with the results.",
+    )
+    parser.add_argument(
+        "--pdf",
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="Generate a PDF with the results.",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="Show verbose output.",
+    )
+    parser.add_argument(
+        "--filter",
+        help='Filter sites to be searched by list property value.E.g --filter "cat=social"',
+    )
+    parser.add_argument(
+        "--no-nsfw", action="store_true", help="Removes NSFW sites from the search."
+    )
+    parser.add_argument(
+        "--dump", action="store_true", help="Dump HTML content for found accounts."
+    )
+    parser.add_argument("--proxy", help="Proxy to send HTTP requests though.")
+    parser.add_argument(
+        "--timeout",
+        type=int,
+        default=30,
+        help="Timeout in seconds for each HTTP request (Default is 30).",
+    )
+    parser.add_argument(
+        "--max-concurrent-requests",
+        type=int,
+        default=30,
+        help="Specify the maximum number of concurrent requests allowed. Default is 30.",
+    )
+    parser.add_argument(
+        "--no-update", action="store_true", help="Don't update sites lists."
+    )
+    parser.add_argument(
+        "--about", action="store_true", help="Show about information and exit."
+    )
+    args = parser.parse_args()
+
+    # Store the necessary arguments to config Object
+    config.username = args.username
+    config.username_file = args.username_file
+    config.permute = args.permute
+    config.permuteall = args.permuteall
+    config.csv = args.csv
+    config.pdf = args.pdf
+    config.filter = args.filter
+    config.no_nsfw = args.no_nsfw
+    config.dump = args.dump
+    config.proxy = args.proxy
+    config.verbose = args.verbose
+    config.timeout = args.timeout
+    config.max_concurrent_requests = args.max_concurrent_requests
+    config.email = args.email
+    config.email_file = args.email_file
+    config.no_update = args.no_update
+    config.about = args.about
+    config.instagram_session_id = os.getenv("INSTAGRAM_SESSION_ID")
+
+    config.console = Console()
+
+    config.dateRaw = datetime.now().strftime("%m_%d_%Y")
+    config.datePretty = datetime.now().strftime("%B %d, %Y")
+
+    config.userAgent = getRandomUserAgent(config)
+
+    config.usernameFoundAccounts = None
+    config.emailFoundAccounts = None
+
+    config.currentUser = None
+    config.currentEmail = None
+
